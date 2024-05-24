9c04ad0b639256c399cf977525d405f75b12ea79
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-05-24 14:00:43 +0530

    create route structure

---
+++ backend/db.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+// Connect db
+
+mongoose.connect('mongodb://localhost:27017/paytm')
+
+// Schema
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 4,
+    maxLength: 10,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+})
+
+// Model
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+  User,
+}
