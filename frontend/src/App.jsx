b9ef742d365e42b5f973d284353bc4d5a8773052
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-06-03 04:11:54 +0530

    signup connected with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Heading } from "./components/Heading";
-import { Signup } from "./pages/Signup";
-import { Signin } from "./pages/Signin";
-import { Dashboard } from "./pages/Dashboard";
-import { SendMoney } from "./pages/SendMoney";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Heading } from './components/Heading'
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Dashboard } from './pages/Dashboard'
+import { SendMoney } from './pages/SendMoney'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Heading label={"Home Page"} />} />
+        <Route path="/" element={<Heading label={'Home Page'} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/sendMoney" element={<SendMoney />} />
+        <Route path="/send" element={<SendMoney />} />
 
-
-        
-
-        <Route path="*" element={<Heading label={"404 Not Found"} />} />
+        <Route path="*" element={<Heading label={'404 Not Found'} />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
