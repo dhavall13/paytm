0ade73128e06c9381fb7ecff672a282f528d6597
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-06-03 00:40:54 +0530

    simple ui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Heading } from "./components/Heading";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Heading label={"Home Page"} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sendMoney" element={<SendMoney />} />
+
+
+        
+
+        <Route path="*" element={<Heading label={"404 Not Found"} />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
